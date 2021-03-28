bada2eff9241a9be0f4397c1874e97e1f689cf14
liumingxing <1421053434@qq.com>
2021-03-28 17:34:17 +0800

    完成目标检测的测试

--- save_model.py
+++ save_model.py
@@ -8,6 +8,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 model = DarkNet()
 # 编译网络，生成weights参数结构
 model(tf.keras.layers.Input(shape=(608, 608, 3)))
+# 打印出网络结构
+print(model.summary())
 # 解析权重参数
 yolo_weights = parse_yolo_v2_model_weights(model.weight_shapes, 'yolov2_weights/yolov2.weights')
 # 将权重参数导入模型
